3ecc9c6f7e41de50c463afc5fc706e1ea922261a
chetannn-github <mr.csr69@gmail.com>
2024-10-05 16:03:26 +0530

    setup - appwrite for authentication , db and storage.

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,19 +1,35 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { Link, router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Logo from "../../assets/images/logo.png"
 import eyehide from "../../assets/icons/eye-hide.png"
 import eye from "../../assets/icons/eye.png"
+import { signin } from '../../lib/appwrite';
 
 const Login = () => {
   let [showPassword , setShowPassword] = useState(false);
-  let handlePress = () =>{
-    router.push("/(auth)/signup")
-}
-let toggleShowPassword = () =>{
+  let inputs = useRef({email:"",password:""});
+  
+  let toggleShowPassword = () =>{
   setShowPassword(!showPassword);
-}
+  }
+
+  let handleLogin = async() =>{
+    if (!inputs.current.email || !inputs.current.password) {
+      Alert.alert('Error', 'Please fill in all fields!');
+    } else {
+      try {
+         await signin(inputs.current.email,inputs.current.password);
+      router.navigate("/home")
+      } catch (error) {
+        throw new Error (error)
+      }
+     
+    }
+
+  }
+
 
   return (
     <View className=" bg-[#161622] flex-1  h-full w-full items-center pt-[15%] "  >
@@ -27,19 +43,19 @@ let toggleShowPassword = () =>{
             
             <View className="flex gap-2">
                   <Text className="text-lg text-[#cdcde0]">email</Text>
-                  <TextInput placeholder='your email address'  placeholderTextColor={"#7B7B8B"} className="h-[55px] text-base rounded-lg bg-[#1E1E2D] px-4"/>
+                  <TextInput placeholder='your email address'   onChangeText={(text) => inputs.current.email = text}  placeholderTextColor={"#7B7B8B"} className="focus:border-secondary text-white  focus:border-2 h-[55px] text-base rounded-lg bg-[#1E1E2D] px-4"/>
             </View>
             <View className="flex gap-1 ">
                   <Text className="text-lg text-[#cdcde0]">password</Text>
                   
                   <View className="flex flex-row justify-between">
-                    <TextInput secureTextEntry={true && !showPassword} placeholder='enter your password'  placeholderTextColor={"#7B7B8B"} className="h-[55px] w-[100%] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
+                    <TextInput onChangeText={(text) => inputs.current.password = text} secureTextEntry={true && !showPassword} placeholder='enter your password'  placeholderTextColor={"#7B7B8B"} className="focus:border-secondary focus:border-2 h-[55px] w-[100%] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
                     <TouchableOpacity className="w-10 -translate-x-10  flex items-center justify-center " onPress={toggleShowPassword}><Image className="w-[25px] h-[25px]" resizeMode='contain' source={showPassword? eyehide:eye}></Image></TouchableOpacity>
                   </View>
                   
             </View>
 
-          <TouchableOpacity onPress={handlePress} className=" py-3 px-2  rounded-lg  flex justify-center items-center font-bold  text-[#161622] bg-[#ff8c00]">
+          <TouchableOpacity onPress={handleLogin} className=" py-3 px-2  rounded-lg  flex justify-center items-center font-bold  text-[#161622] bg-[#ff8c00]">
               <Text className="text-lg font-semibold" >Log In</Text>
             </TouchableOpacity>
 
--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,18 +1,26 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { Link, router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Logo from "../../assets/images/logo.png"
 import eyehide from "../../assets/icons/eye-hide.png"
 import eye from "../../assets/icons/eye.png"
+import { createUser, signin } from '../../lib/appwrite';
 
 
 const Signup = () => {
   let [showPassword , setShowPassword] = useState(false);
-  let handlePress = () =>{
-    router.push("/(auth)/login")
+  
+  let inputs = useRef({email:"",password:"",username:""});
 
-  }
+  let handleSignup = async() => {
+    if (!inputs.current.email || !inputs.current.password || !inputs.current.username) {
+      Alert.alert('Error', 'Please fill in all fields!');
+    } else {
+      await signin(inputs.current.email,inputs.current.password,inputs.current.username);
+      router.navigate("/home")
+    }
+  };
   let toggleShowPassword = () =>{
     setShowPassword(!showPassword);
   }
@@ -29,23 +37,23 @@ const Signup = () => {
 
             <View className="flex gap-y-2 ">
               <Text className="text-lg text-[#cdcde0]">username</Text>
-              <TextInput placeholder='your unique username'  placeholderTextColor={"#7B7B8B"} className="h-[55px] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
+              <TextInput onChangeText={(text) => inputs.current.username = text} placeholder='your unique username'  placeholderTextColor={"#7B7B8B"} className="focus:border-secondary focus:border-2 h-[55px] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
             </View>
             <View className="flex gap-2">
                   <Text className="text-lg text-[#cdcde0]">email</Text>
-                  <TextInput placeholder='your email address'  placeholderTextColor={"#7B7B8B"} className="h-[55px] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
+                  <TextInput onChangeText={(text) => inputs.current.email = text}  placeholder='your email address'  placeholderTextColor={"#7B7B8B"} className="focus:border-secondary focus:border-2 h-[55px] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
             </View>
             <View className="flex gap-1 ">
                   <Text className="text-lg text-[#cdcde0]">password</Text>
                   
                   <View className="flex flex-row justify-between">
-                    <TextInput secureTextEntry={true && !showPassword} placeholder='enter your password'  placeholderTextColor={"#7B7B8B"} className="h-[55px] w-[100%] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
+                    <TextInput  onChangeText={(text) => inputs.current.password = text} secureTextEntry={true && !showPassword} placeholder='enter your password'  placeholderTextColor={"#7B7B8B"} className="focus:border-2 focus:border-secondary h-[55px] w-[100%] text-white text-base rounded-lg bg-[#1E1E2D] px-4"/>
                     <TouchableOpacity className="w-10 -translate-x-10  flex items-center justify-center " onPress={toggleShowPassword}><Image className="w-[25px] h-[25px]" resizeMode='contain' source={showPassword? eyehide:eye}></Image></TouchableOpacity>
                   </View>
                   
             </View>
 
-          <TouchableOpacity onPress={handlePress} className=" py-3 px-2  rounded-lg  flex justify-center items-center font-bold  text-[#161622] bg-[#ff8c00]">
+          <TouchableOpacity onPress={handleSignup} className=" py-3 px-2  rounded-lg  flex justify-center items-center font-bold  text-[#161622] bg-[#ff8c00]">
               <Text className="text-lg font-semibold" >Sign Up</Text>
             </TouchableOpacity>
 
